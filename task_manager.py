d486ce2896555dfbfbbcc17755a3951b62c36955
Ocean <oceaniyer9@gmailcom>
2025-09-11 16:21:45 +0200

    Updated cleaner version

--- task_manager.py
+++ task_manager.py
@@ -9,22 +9,7 @@ except FileNotFoundError:
     tasks = []
     print("No existing task file found. A new one will be created.")
 
-
-while True:
-    command = input("Enter a command (help to list commands): ").strip().lower()
-
-    if command == "help":
-        print("Available commands:")
-        print("help - Show this help message")
-        print("add - Add a new task")
-        print("edit - Edit an existing task")
-        print("delete - Delete a task")
-        print("list - List all tasks")
-        print("exit - Exit the program")
-    elif command == "exit":
-        print("Exiting the program.")
-        exit()
-    elif command == "add":
+def add_task(tasks):
         title = input("Enter task title: ")
         status = input("Enter task status (completed/pending): ")
         description = input("Enter task description: ")
@@ -48,8 +33,14 @@ while True:
         with open("task.json", "w") as json_file:
             json.dump(tasks, json_file, indent=4)
             print("Task added successfully.")
-    elif command == "edit":
-            id = int(input("Enter task ID to edit: "))
+def edit_task(tasks):
+            for task in tasks:
+                 print(f"ID: {task['id']}, {task['title']}")
+            raw_input = input("Enter task ID to edit: ")
+            if raw_input == 'back':
+                 return
+            else:
+                 id = int(raw_input)
             if not any(task['id'] == id for task in tasks):
                 print("Task ID not found.")
             else:
@@ -72,7 +63,18 @@ while True:
                                 json.dump(tasks, json_file, indent=4)
                                 print("Task updated successfully.")
                             break
-    elif command == "delete":
+def help_menu(tasks):
+        print("Available commands:")
+        print("help - Show this help message")
+        print("add - Add a new task")
+        print("edit - Edit an existing task")
+        print("delete - Delete a task")
+        print("list - List all tasks")
+        print("exit - Exit the program")
+def exit_program(tasks):
+        print("Exiting the program.")
+        exit()
+def delete_task(tasks):
         print("Current tasks:", [f"ID: {task['id']}, Title: {task['title']}" for task in tasks])
         id = int(input("Enter task ID to delete: "))
         for task in tasks:
@@ -84,28 +86,39 @@ while True:
                     break
         else:
             print("Task ID not found.")
-
-    elif command == "list":
-        if tasks == []:
-            print("No tasks available.")
-        else:
-            type = input("Enter type of tasks to list (all/completed/pending): ").strip().lower()
-            if type == "all":
-                for task in tasks:
-                    print(f"ID: {task['id']}, Title: {task['title']}, Status: {task['status']}, Description: {task['description']}, Created At: {task['created_at']}")
-            elif type == "completed":
-                for task in tasks:
-                    if task['status'].lower() == 'completed':
-                        print(f"ID: {task['id']}, Title: {task['title']}, Status: {task['status']}, Description: {task['description']}, Created At: {task['created_at']}")
-                else:
-                    print("No completed tasks available.")
-            elif type == "pending":
-                for task in tasks:
-                    if task['status'].lower() == 'pending':
-                        print(f"ID: {task['id']}, Title: {task['title']}, Status: {task['status']}, Description: {task['description']}, Created At: {task['created_at']}")
-                else:
-                    print("No pending tasks available.")
+def list_tasks(tasks): 
+            if tasks == []:
+                print("No tasks available.")
             else:
-                print("Invalid type. Please enter all, completed, or pending.")
+                type = input("Enter type of tasks to list (all/completed/pending/back): ").strip().lower()
+                if type == "all":
+                    for task in tasks:
+                        print(f"ID: {task['id']}, Title: {task['title']}, Status: {task['status']}, Description: {task['description']}, Created At: {task['created_at']}")
+                elif type == "completed":
+                    for task in tasks:
+                        if task['status'].lower() == 'completed':
+                            print(f"ID: {task['id']}, Title: {task['title']}, Status: {task['status']}, Description: {task['description']}, Created At: {task['created_at']}")
+                elif type == "pending":
+                    for task in tasks:
+                        if task['status'].lower() == 'pending':
+                            print(f"ID: {task['id']}, Title: {task['title']}, Status: {task['status']}, Description: {task['description']}, Created At: {task['created_at']}")   
+                else:
+                    print("Invalid type. Please enter all, completed, or pending.")
+
+
+command_dictionary = {
+     "add": add_task,
+     "edit": edit_task,
+     "help": help_menu,
+     "exit": exit_program,
+     "delete": delete_task,
+     "list": list_tasks
+}
+
+while True:
+    command = input("Enter a command (help to list commands): ").strip().lower()
+    
+    if command in command_dictionary:
+         command_dictionary[command](tasks)
     else:
-        print("Invalid command. Type 'help' to see the list of commands.")
+         print("Invalid Command")
